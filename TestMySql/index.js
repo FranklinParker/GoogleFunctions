3b634f8eb83810d2cb26c2836fc79d1fcfd784a9
FranklinParker <fparker@deliveryman.com>
2018-09-07 15:59:58 -0400

    find all

--- TestMySql/index.js
+++ TestMySql/index.js
@@ -3,7 +3,7 @@
 // than grant privalages via tool
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('world', 'foo','bar', {
+const sequelize = new Sequelize('world', 'foo', 'bar', {
     host: '127.0.0.1',
     dialect: 'mysql',
 
@@ -24,11 +24,62 @@ const User = sequelize.define('user', {
     birthday: Sequelize.DATE
 });
 
-sequelize.sync()
-    .then(() => User.create({
-        username: 'janedoe',
-        birthday: new Date(1980, 6, 20)
-    }))
-    .then(jane => {
-        console.log(jane.toJSON());
+const createUser = () => {
+    sequelize.sync()
+        .then(() => User.create({
+            username: 'janedoe',
+            birthday: new Date(1980, 6, 20)
+        }))
+        .then(jane => {
+            console.log(jane.toJSON());
+        });
+}
+
+
+/**
+ *
+ * CountryCode char(3) PK
+ Language char(30) PK
+ IsOfficial enum('T','F')
+ Percentage float(4,1)
+ * @type {Model}
+ */
+
+const CountryCode = sequelize.define('CountryLanguage', {
+        CountryCode: Sequelize.STRING(3),
+        Language: Sequelize.STRING(30),
+        IsOfficial: Sequelize.ENUM('T', 'F'),
+        Percentage: Sequelize.FLOAT(4, 1)
+    },
+    {
+        // don't add the timestamp attributes (updatedAt, createdAt)
+        timestamps: false,
+
+        // don't delete database entries but set the newly added attribute deletedAt
+        // to the current date (when deletion was done). paranoid will only work if
+        // timestamps are enabled
+        paranoid: true,
+
+        // don't use camelcase for automatically added attributes but underscore style
+        // so updatedAt will be updated_at
+        //   underscored: true,
+
+        // disable the modification of tablenames; By default, sequelize will automatically
+        // transform all passed model names (first parameter of define) into plural.
+        // if you don't want that, set the following
+        freezeTableName: true
+
+        // define the table's name
+        // tableName: 'my_very_custom_table_name'
     });
+
+const findAllCountryCode = () => {
+    CountryCode.findAll(
+        {
+            attributes: ['CountryCode', 'Language', 'IsOfficial', 'Percentage']
+        }).then((records) => {
+        console.log('records', records);
+    });
+}
+
+findAllCountryCode();
