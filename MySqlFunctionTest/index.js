ae37829f840cb84797c7c869e9782691ffb2604a
FranklinParker <fparker@deliveryman.com>
2018-09-10 14:31:50 -0400

    fix

--- MySqlFunctionTest/index.js
+++ MySqlFunctionTest/index.js
@@ -1,9 +1,15 @@
-onst mysql = require('mysql2');
+const mysql = require('mysql');
 
 const connectionName = process.env.DB_INSTANCE;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
+
+console.log('dbUser:' + dbUser);
+console.log('dbPass:' + dbPass);
+console.log('connectionName:' + connectionName);
+console.log('dbName:' + dbName);
+
 
 const pool = mysql.createPool({
     connectionLimit : 1,
@@ -13,11 +19,11 @@ const pool = mysql.createPool({
     database: dbName
 });
 
-exports.cloudSQLTest = (req, res) => {
+exports.cloudSQLTest = function (req, res) {
     pool.query('SELECT NOW() AS now', (error, results, fields) => {
         console.log('query results');
         if(error){
-            res.status(403).send('error testing db', error);
+             res.status(200).send('error testing db', error);
         }else{
             res.status(200).send(' query results', results);
         }
