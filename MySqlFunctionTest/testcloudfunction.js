77ac85f6668abd711390666ca6e6b6ed978c7d6e
FranklinParker <fparker@deliveryman.com>
2018-09-12 11:09:10 -0400

    use cloud function to my sql

---
+++ MySqlFunctionTest/testcloudfunction.js
@@ -0,0 +1,33 @@
+
+const mysql = require('mysql');
+
+const connectionName = process.env.DB_INSTANCE;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+console.log('dbUser:' + dbUser);
+console.log('dbPass:' + dbPass);
+console.log('connectionName:' + connectionName);
+console.log('dbName:' + dbName);
+
+
+const pool = mysql.createPool({
+    connectionLimit : 1,
+    socketPath: '/cloudsql/' + connectionName,
+    user: dbUser,
+    password: dbPass,
+    database: dbName
+});
+
+exports.cloudSQLTest = function (req, res) {
+    pool.query('SELECT NOW() AS now', (error, results, fields) => {
+        console.log('query results', results);
+        console.log('query error', error);
+        if(error){
+            res.status(200).send('error testing db', error);
+        }else{
+            res.status(200).send(' query results', results);
+        }
+    });
+};
