e3b557e82725cb9adc3970e2d15108595a7eb4cc
FranklinParker <fparker@deliveryman.com>
2018-09-12 13:49:44 -0400

    create a config object

--- MySqlFunctionTest/index.js
+++ MySqlFunctionTest/index.js
@@ -5,19 +5,17 @@ const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 
-console.log('dbUser:' + dbUser);
-console.log('dbPass:' + dbPass);
-console.log('connectionName:' + connectionName);
-console.log('dbName:' + dbName);
 
-
-const pool = mysql.createPool({
+const config = {
     connectionLimit : 1,
     socketPath: '/cloudsql/' + connectionName,
     user: dbUser,
     password: dbPass,
     database: dbName
-});
+};
+
+console.log('DBConfig:', config);
+const pool = mysql.createPool(config);
 
 exports.cloudSQLTest = function (req, res) {
     pool.query('SELECT NOW() AS now', (error, results, fields) => {
