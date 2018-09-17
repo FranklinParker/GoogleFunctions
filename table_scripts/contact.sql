15d400a0bb473622cb7f0fcbe33288228456caa8
FranklinParker <fparker@deliveryman.com>
2018-09-17 15:37:03 -0400

    tabke

--- table_scripts/contact.sql
+++ table_scripts/contact.sql
@@ -1,15 +1,14 @@
-drop table contact_new;
+drop table contact;
 
-create table contact_new(
-     id INT NOT NULL AUTO_INCREMENT,
-     firstName VARCHAR(50) NOT NULL,
-     lastName VARCHAR(50) NOT NULL,
-     email VARCHAR(100) NOT NULL unique,
-     address VARCHAR(50) ,
-     city VARCHAR(50) ,
-     state VARCHAR(50),
-     zip VARCHAR(50) ,
-     PRIMARY KEY ( id )
-  );
-  
-  
+  CREATE TABLE `contact` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `firstName` varchar(50) NOT NULL,
+    `lastName` varchar(50) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `address` varchar(50) DEFAULT NULL,
+    `city` varchar(50) DEFAULT NULL,
+    `state` varchar(50) DEFAULT NULL,
+    `zip` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
