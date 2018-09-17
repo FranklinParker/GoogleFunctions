58053a458d35e0e89f64e9fa2231e9b70b9c0f60
FranklinParker <fparker@deliveryman.com>
2018-09-17 10:40:08 -0400

    auth header

--- TestSequelizeGoogleFunction/auth/index.js
+++ TestSequelizeGoogleFunction/auth/index.js
@@ -1,33 +1,39 @@
+const authHeaderName = process.env.AUTH_HEADER_NAME || 'x-auth';
+
 /**
  * if auth methods exists confirm required info exists
  *
  * @param params
  * @param authMethods
  */
-const checkIfAuthenticateRequired = (params, authMethods)=>{
-  if(!authMethods){
-    return;
-  }
-  try{
-    authMethods.forEach(authMethod=>authMethod(params))
-  } catch (e){
-    throw e;
-  }
+const checkIfAuthenticateRequired = (params, authMethods) => {
+
+    if (!authMethods) {
+        return;
+    }
+    try {
+        authMethods.forEach(authMethod => authMethod(params))
+    } catch (e) {
+        throw e;
+    }
 }
 /**
  *
  *
  * @param params
  */
-const loginAuth = (params)=>{
-  console.log('login auth', params);
-  if(!params || params.username!=='test'){
-    throw new Error('not authenticated');
-  }
+const loginAuth = (params) => {
+    console.log('login auth', params);
+    const authHeader = params.headers[authHeaderName];
+    console.log('authheader:' + authHeader);
+    if (!authHeader || authHeader !== 'test') {
+        throw new Error('not authenticated');
+    }
+
 }
 
 
-module.exports=  {
-  loginAuth,
-  checkIfAuthenticateRequired
+module.exports = {
+    loginAuth,
+    checkIfAuthenticateRequired
 }
--- TestSequelizeGoogleFunction/localtest.js
+++ TestSequelizeGoogleFunction/localtest.js
@@ -1,33 +1,37 @@
 const contactservice = require('./services/contactservice');
 const userservice = require('./services/userservice');
-const { process} = require('./routing');
+const {process} = require('./routing');
 
-const testContactGetAll = async ()=> {
+const testContactGetAll = async () => {
     const result = await contactservice.getContacts();
     console.log('get contacts', result);
 }
 
-const testRegisterUser = async ()=>{
-    const result = await userservice.register('jay','jeff','test8', 'test7pw');
+const testRegisterUser = async () => {
+    const result = await userservice.register('jay', 'jeff', 'test8', 'test7pw');
     console.log('register result', result);
 }
 
-const testGetUsers  = async ()=> {
-  const result = await userservice.getAllUsers();
-  console.log('get users', result);
+const testGetUsers = async () => {
+    const result = await userservice.getAllUsers();
+    console.log('get users', result);
 }
 
-const testRouting =  async (key, params)=>{
-  const result = await process(key, params);
-  console.log(`${key} result ${JSON.stringify(result)}`);
+const testRouting = async (key, params) => {
+    const result = await process(key, params);
+    console.log(`${key} result ${JSON.stringify(result)}`);
 
 }
 //testContactGetAll();
 //testGetUsers();
 //testRegisterUser();
-testRouting('/register?name=test',{ body:{ email:"username3",
-  firstName: "jill" ,
-  lastName: "george",
-  password: "testtest"}
+testRouting('/register?name=test', {
+    body: {
+        email: "username3",
+        firstName: "jill",
+        lastName: "george",
+        password: "testtest"
+    },
+    headers: {'x-auth': 'test'}
 });
 
