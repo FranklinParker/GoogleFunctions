77205850c90fa806b5990debea7ebf2771512639
Franklin Parker <fparker@deliveryman.com>
2018-09-17 20:06:37 -0400

    fix table create

--- table_scripts/contact.sql
+++ table_scripts/contact.sql
@@ -11,7 +11,7 @@ drop table contact;
     `zip` varchar(50) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `email` (`email`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  ) ;
 
 
 INSERT INTO contact (firstName,lastName,email) VALUES('jill','jones','jj@aol.com');
